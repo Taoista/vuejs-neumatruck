a32357f2e7433771c3c5f36af30c2c8e33420377
taoista <taoista-@hotmail.com>
2024-07-22 05:33:52 -0400

    curso de VEUJS

---
+++ src/components/Contador.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <p>{{ contador }}</p>
+  
+    <button @click="incrementar">incrementar</button>
+  
+    <button @click="restart">Resar</button>
+  
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+  
+    const contador = ref(0)
+    
+    const incrementar = () => {
+      contador.value++
+    }
+  
+    const restart = () => {
+      
+      if(contador.value==0){
+        return false;
+      }
+  
+      contador.value--
+    }
+  
+  </script>
+  
+  <style scoped>
+    p{
+      color: red;
+    }
+  </style>
+  
